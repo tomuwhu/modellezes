e2854e479d90749219e41d97977906bc82bf72c8
Tamás Németh <tomuwhu@gmail.com>
2019-02-06 12:56:53 +0100

    .

--- ablak/src/views/Home.vue
+++ ablak/src/views/Home.vue
@@ -3,8 +3,8 @@
     <h1>Mozgás</h1>
     <button v-if="!started" @click="start()">Start</button>
     <button v-if="started" @click="stop()">Stop</button>
-    <button @click="gravity=!gravity">Gravity</button>
-    <input type="range" v-model="av">
+    <button @click="gravity=!gravity">Gravity</button> - 
+    <input type="range" min=-10 max=10 v-model="av"> {{av}}
     <svg viewBox="0 0 1000 700">
       <circle :cx="k1xp" :cy="k1yp" r="20"/>
     </svg>
@@ -28,21 +28,22 @@ export default {
   methods: {
     start() {
       this.started=true
-      interval=setInterval(() => {
-        this.k1xp+=this.k1vx
-        this.k1yp+=this.k1vy
-        if (this.k1xp>980) this.k1vx = -this.k1vx
-        if (this.k1xp<20) this.k1vx = -this.k1vx
-        if (this.k1yp>680) this.k1vy = -this.k1vy
-        if (this.k1yp<20) this.k1vy = -this.k1vy
-        if (this.gravity) this.k1vy+=0.05
-        this.k1vx=this.k1vx*(1-this.av/10000)
-        this.k1vy=this.k1vy*(1-this.av/10000)
-      },30)
+      interval=setInterval(this.mozog ,30)
     },
     stop() {
       this.started=false
       clearInterval(interval)
+    },
+    mozog() {
+      this.k1xp+=this.k1vx
+      this.k1yp+=this.k1vy
+      if (this.k1xp>980) this.k1vx = -this.k1vx
+      if (this.k1xp<20) this.k1vx = -this.k1vx
+      if (this.k1yp>680) this.k1vy = -this.k1vy
+      if (this.k1yp<20) this.k1vy = -this.k1vy
+      if (this.gravity) this.k1vy+=0.05
+      this.k1vx=this.k1vx*(1+this.av/1000)
+      this.k1vy=this.k1vy*(1+this.av/1000)
     }
   }
 }
